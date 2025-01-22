e89cf5d3a40ddfcca44cac0eeb3586ce89b9b7ba
ryuseunghan <jorzor99@gmail.com>
2025-01-22 16:18:44 +0900

    mutating data

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,4 +1,6 @@
 import clsx from 'clsx';
+import { deleteInvoice } from '@/app/lib/actions';
+
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
